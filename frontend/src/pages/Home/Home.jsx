1fb36822f09658bb1dc8650b70f04ad84c193c08
Gabriel Souza <eugabrielf@gmail.com>
2022-06-25 17:13:59 -0300

    criando pagina home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -31,12 +31,64 @@ const PagesHome = () => {
           </li>
         </ul>
       </div>
-      <div className="content">content</div>
-      <div className="footer">
-        <p>
-          Desenvolvido com <i class="fas fa-heart"/> por Gabriel Souza.
-        </p>
+      <div className="content">
+        <div className="container">
+          <div className="search">
+            <input
+              placeholder="Pesquisa por usuário...."
+            />
+            <i class="fas fa-search"></i>
+          </div>
+
+          <div className="container-user">
+            <p>Gabriel Souza de Figueredo </p>
+            <div className="container-buttons">
+              <button>
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+              <button>
+                <i class="fas fa-edit"></i>
+              </button>
+              <button>
+                <i className="fas fa-user" />
+              </button>
+            </div>
+          </div>
+          <div className="container-user">
+            <p>Erica Santos </p>
+            <div className="container-buttons">
+              <button>
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+              <button>
+                <i class="fas fa-edit"></i>
+              </button>
+              <button>
+                <i className="fas fa-user" />
+              </button>
+            </div>
+          </div>
+          <div className="container-user">
+            <p>João Felipe </p>
+            <div className="container-buttons">
+              <button>
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </button>
+              <button>
+                <i class="fas fa-edit"></i>
+              </button>
+              <button>
+                <i className="fas fa-user" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+      {/* <div className="footer">
+        <p>
+          Desenvolvido com <i class="fas fa-heart" /> por Gabriel Souza.
+        </p>
+      </div> */}
     </div>
   );
 };
