ee95d35cc91efcf5ae624b218e484800dbfdc6f3
Gabriel Souza <eugabrielf@gmail.com>
2022-06-27 14:00:21 -0300

    implementando pagina de cadastro

--- frontend/src/components/FormRegisterUser/FormRegister.jsx
+++ frontend/src/components/FormRegisterUser/FormRegister.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Service from "../../service/service";
 import "./styles.css";
 
 function FormRegisterUser() {
-  const handleSubmit = (e) => {
+  const [user, setUser] = useState({
+    nome: "",
+    dataNas: "",
+    cpf: "",
+    celular: "",
+    telefone: "",
+    cep: "",
+    complemente: "",
+    logradouro: "",
+    bairro: "",
+    estado: "",
+    numero: "",
+    cidade: "",
+  });
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      await Service.post("users/", user);
+      history.push("/usuarios");
+    } catch (error) {
+      console.log("error em fazer post...");
+    }
   };
 
   return (
@@ -16,26 +46,56 @@ function FormRegisterUser() {
           <div className="group-input">
             <div className="container-input">
               <label>Nome</label>
-              <input placeholder="Gabriel Souza" className="input-large" />
+              <input
+                name="nome"
+                placeholder="Gabriel Souza"
+                className="input-large"
+                value={user.nome}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Data Nascimento</label>
-              <input placeholder="20/03/1995" className="input-medium" />
+              <input
+                name="dataNas"
+                placeholder="20/03/1995"
+                className="input-medium"
+                value={user.dataNas}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>CPF</label>
-              <input placeholder="000.000.000-88" className="input-medium" />
+              <input
+                name="cpf"
+                placeholder="000.000.000-88"
+                className="input-medium"
+                value={user.cpf}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="group-input">
             <div className="container-input">
               <label>Telefone</label>
-              <input placeholder="3457-8907" className="input-medium" />
+              <input
+                name="telefone"
+                placeholder="3457-8907"
+                className="input-medium"
+                value={user.telefone}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Celular</label>
-              <input placeholder="(61) 99458-9078" className="input-medium" />
+              <input
+                name="celular"
+                placeholder="(61) 99458-9078"
+                className="input-medium"
+                value={user.celular}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
@@ -44,41 +104,85 @@ function FormRegisterUser() {
           <div className="group-input">
             <div className="container-input">
               <label>Cep</label>
-              <input placeholder="72341-402" className="input-medium" />
+              <input
+                name="cep"
+                placeholder="72341-402"
+                className="input-medium"
+                value={user.cep}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Logradouro</label>
-              <input placeholder="QR 209 Conjunto 2" className="input-medium" />
+              <input
+                name="logradouro"
+                placeholder="QR 209 Conjunto 2"
+                className="input-medium"
+                value={user.logradouro}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Complemente</label>
-              <input placeholder="000.000.000-88" className="input-medium" />
+              <input
+                name="complemente"
+                placeholder="000.000.000-88"
+                className="input-medium"
+                value={user.complemente}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="group-input">
             <div className="container-input">
               <label>Numero</label>
-              <input placeholder="06" className="input-medium" />
+              <input
+                name="numero"
+                placeholder="06"
+                className="input-medium"
+                value={user.numero}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Bairro</label>
-              <input placeholder="Samambaia Norte (Samambaia)" className="input-medium" />
+              <input
+                name="bairro"
+                placeholder="Samambaia Norte (Samambaia)"
+                className="input-medium"
+                value={user.bairro}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Cidade</label>
-              <input placeholder="Brasília" className="input-medium" />
+              <input
+                name="cidade"
+                placeholder="Brasília"
+                className="input-medium"
+                value={user.cidade}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="group-input">
             <div className="container-input">
               <label>Estado</label>
-              <input placeholder="DF" className="input-medium" />
+              <input
+                name="estado"
+                placeholder="DF"
+                className="input-medium"
+                value={user.estado}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="container-form-buttons">
-            <Link to="/usuarios" className="button-come-back">Voltar</Link>
+            <Link to="/usuarios" className="button-come-back">
+              Voltar
+            </Link>
             <button className="button-save">Salvar</button>
           </div>
         </form>
