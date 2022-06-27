dd3b7bf3d8702aa1639d686f5502614958b47ad2
Gabriel Souza <eugabrielf@gmail.com>
2022-06-27 17:01:05 -0300

    terminando crud

--- frontend/src/components/FormUserEdit/FormUserEdit.jsx
+++ frontend/src/components/FormUserEdit/FormUserEdit.jsx
@@ -1,10 +1,99 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
+import axios from "axios";
+import Service from "../../service/service";
 import "./styles.css";
 
 function FormUserEdit() {
-  const handleSubmit = (e) => {
+  const [user, setUser] = useState({
+    nome: "",
+    dataNas: "",
+    cpf: "",
+    celular: "",
+    telefone: "",
+    cep: "",
+    complemente: "",
+    logradouro: "",
+    bairro: "",
+    estado: "",
+    numero: "",
+    cidade: "",
+  });
+
+  const history = useHistory();
+  const { id } = useParams();
+
+  const maskInputs = {
+    cpf(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d)/, "$1-$2")
+        .replace(/(-\d{2})(\d)/, "$1");
+    },
+    celular(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{2})(\d)/, "($1) $2")
+        .replace(/(\d{5})(\d)/, "$1-$2")
+        .replace(/(-\d{4})(\d)/, "$1");
+    },
+    cep(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{5})(\d)/, "$1-$2")
+        .replace(/(-\d{3})(\d)/, "$1");
+    },
+    telefone(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{4})(\d)/, "$1-$2")
+        .replace(/(\d{4})-(\d)(\d{4})/, "$1$2-$3")
+        .replace(/(-\d{4})(\d)/, "$1");
+    },
+    dataNas(value) {
+      return value
+        .replace(/\D/g, "")
+        .replace(/(\d{2})(\d)/, "$1/$2")
+        .replace(/(\d{2})(\d)/, "$1/$2")
+        .replace(/(\d{4})(\d)/, "$1");
+    },
+  };
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: maskInputs[e.target.name]
+        ? maskInputs[e.target.name](e.target.value)
+        : e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    async function reqUser() {
+      try {
+        const userData = await Service.get("users/" + id);
+        const response = await userData.data;
+        setUser({ ...user, ...response });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    reqUser();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      await Service.put(`users/${id}`, user);
+      history.push("/usuarios");
+    } catch (error) {
+      console.log("error em fazer put...");
+    }
   };
 
   return (
@@ -16,26 +105,56 @@ function FormUserEdit() {
           <div className="group-input">
             <div className="container-input">
               <label>Nome</label>
-              <input placeholder="Gabriel Souza" className="input-large" />
+              <input
+                name="nome"
+                placeholder="Gabriel Souza"
+                className="input-large"
+                value={user.nome}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Data Nascimento</label>
-              <input placeholder="20/03/1995" className="input-medium" />
+              <input
+                name="dataNas"
+                placeholder="20/03/1995"
+                className="input-medium"
+                value={user.dataNas}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>CPF</label>
-              <input placeholder="000.000.000-88" className="input-medium" />
+              <input
+                name="cpf"
+                placeholder="000.000.000-88"
+                className="input-medium"
+                value={user.cpf}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="group-input">
             <div className="container-input">
               <label>Telefone</label>
-              <input placeholder="3457-8907" className="input-medium" />
+              <input
+                name="telefone"
+                placeholder="3457-8907"
+                className="input-medium"
+                value={user.telefone}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Celular</label>
-              <input placeholder="(61) 99458-9078" className="input-medium" />
+              <input
+                name="celular"
+                placeholder="(61) 99458-9078"
+                className="input-medium"
+                value={user.celular}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
@@ -44,39 +163,78 @@ function FormUserEdit() {
           <div className="group-input">
             <div className="container-input">
               <label>Cep</label>
-              <input placeholder="72341-402" className="input-medium" />
+              <input
+                name="cep"
+                placeholder="72341-402"
+                className="input-medium"
+                value={user.cep}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Logradouro</label>
-              <input placeholder="QR 209 Conjunto 2" className="input-medium" />
+              <input
+                name="logradouro"
+                placeholder="QR 209 Conjunto 2"
+                className="input-medium"
+                value={user.logradouro}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Complemente</label>
-              <input placeholder="000.000.000-88" className="input-medium" />
+              <input
+                name="complemente"
+                placeholder="000.000.000-88"
+                className="input-medium"
+                value={user.complemente}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="group-input">
             <div className="container-input">
               <label>Numero</label>
-              <input placeholder="06" className="input-medium" />
+              <input
+                name="numero"
+                placeholder="06"
+                className="input-medium"
+                value={user.numero}
+                onChange={handleChange}
+              />
             </div>
             <div className="container-input">
               <label>Bairro</label>
               <input
+                name="bairro"
                 placeholder="Samambaia Norte (Samambaia)"
                 className="input-medium"
+                value={user.bairro}
+                onChange={handleChange}
               />
             </div>
             <div className="container-input">
               <label>Cidade</label>
-              <input placeholder="Brasília" className="input-medium" />
+              <input
+                name="cidade"
+                placeholder="Brasília"
+                className="input-medium"
+                value={user.cidade}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="group-input">
             <div className="container-input">
               <label>Estado</label>
-              <input placeholder="DF" className="input-medium" />
+              <input
+                name="estado"
+                placeholder="DF"
+                className="input-medium"
+                value={user.estado}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
